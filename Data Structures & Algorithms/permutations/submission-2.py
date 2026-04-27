1ef3ddcf23bf1be93414e81f4d136ebbbafdb629
Durai85 <mduraimurugan@karunya.edu.in>
2026-04-27 16:32:20 +0530

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res, per = [] , []
+        n = len(nums)
+        def dfs():
+            if len(per) == n:
+                res.append(per.copy())
+                return
+
+            for x in nums:
+                if x not in per:
+                    per.append(x)
+                    dfs()
+                    per.pop()
+
+        dfs()
+        return res
