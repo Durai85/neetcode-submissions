ef61d722f2373452c07d7b851ea753aed15ae49e
Durai85 <mduraimurugan@karunya.edu.in>
2026-07-05 12:08:40 +0530

    Add: dota2-senate - submission-0

---
+++ Data Structures & Algorithms/dota2-senate/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        R = deque()
+        D = deque()
+        n = len(senate)
+
+        for i in range(n):
+            if senate[i] == 'R':
+                R.append(i)
+            else:
+                D.append(i)
+
+        while R and D:
+            r = R.popleft()
+            d = D.popleft()
+
+            if r < d:
+                R.append(r + n)
+            else:
+                D.append(d + n)
+
+        return 'Radiant' if R else 'Dire'
